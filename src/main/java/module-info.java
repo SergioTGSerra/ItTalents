f05cb62c409950502b0196947fd631e10f2020c2
Tiago Serra <sergioserra.2003@gmail.com>
2022-11-22 20:55:12 +0000

    Add Dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,16 @@
 module pt.ipvc.ittalents {
     requires javafx.controls;
     requires javafx.fxml;
+    //Extra
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
 
     opens pt.ipvc.ittalents to javafx.fxml;
     exports pt.ipvc.ittalents;
+    exports pt.ipvc.ittalents.Controllers;
+    exports pt.ipvc.ittalents.Models;
+    exports pt.ipvc.ittalents.Views;
+
 }
