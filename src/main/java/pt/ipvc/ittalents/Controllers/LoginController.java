c49a2ae87af616553c9575120ec3b92f4deb589a
Tiago Serra <sergiserra.2003@gmail.com>
2022-11-28 09:46:48 +0000

    Update LoginController

--- src/main/java/pt/ipvc/ittalents/Controllers/LoginController.java
+++ src/main/java/pt/ipvc/ittalents/Controllers/LoginController.java
@@ -1,4 +1,18 @@
 package pt.ipvc.ittalents.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import pt.ipvc.ittalents.Views.ViewFactory;
+
 public class LoginController {
+    public TextField username;
+    public PasswordField password;
+    public Button goToRegisterBtn;
+
+    public void goToRegister() {
+        ViewFactory.closeStage((Stage)goToRegisterBtn.getScene().getWindow());
+        ViewFactory.showRegister();
+    }
 }
