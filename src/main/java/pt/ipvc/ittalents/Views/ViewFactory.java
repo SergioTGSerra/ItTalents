ab1ffa3a9b21bd97ec31b702f194a4ae4d9a3053
Tiago Serra <sergiserra.2003@gmail.com>
2022-11-28 09:45:30 +0000

    Creates Methods in View Factory

--- src/main/java/pt/ipvc/ittalents/Views/ViewFactory.java
+++ src/main/java/pt/ipvc/ittalents/Views/ViewFactory.java
@@ -1,2 +1,35 @@
-package pt.ipvc.ittalents.Views;public class ViewFactory {
+package pt.ipvc.ittalents.Views;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ViewFactory {
+
+    public static void showLogin() {
+        createStage("Login");
+    }
+
+    public static void showRegister() {
+        createStage("Register");
+    }
+
+    public static void closeStage(Stage stage){
+        stage.close();
+    }
+
+    private static void createStage(String file){
+        FXMLLoader fxmlLoader = new FXMLLoader(ViewFactory.class.getResource("/pt/ipvc/ittalents/Fxml/" + file + ".fxml"));
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Stage stage = new Stage();
+        stage.setTitle("ItTalents - " + file);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
