c5986adcb5f49bf0a2da97b2683d801cc5f025c4
Tiago Serra <sergiserra.2003@gmail.com>
2022-11-28 09:46:11 +0000

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,5 @@ module pt.ipvc.ittalents {
     opens pt.ipvc.ittalents to javafx.fxml;
     exports pt.ipvc.ittalents;
     exports pt.ipvc.ittalents.Controllers;
-    exports pt.ipvc.ittalents.Models;
     exports pt.ipvc.ittalents.Views;
-
 }
