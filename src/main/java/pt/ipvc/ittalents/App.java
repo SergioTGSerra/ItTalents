e0ab37952b8f5998e2315f7b7ad680e7d942d18d
Tiago Serra <sergiserra.2003@gmail.com>
2022-11-28 09:44:41 +0000

    Update App.java

--- src/main/java/pt/ipvc/ittalents/App.java
+++ src/main/java/pt/ipvc/ittalents/App.java
@@ -1,20 +1,13 @@
 package pt.ipvc.ittalents;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import pt.ipvc.ittalents.Views.ViewFactory;
 
 public class App extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Fxml/Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        ViewFactory.showRegister();
     }
 
     public static void main(String[] args) {
