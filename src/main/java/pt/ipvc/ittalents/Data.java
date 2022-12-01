94fa4864adcf01376ce194d0af038c46c4971cd2
tiago <sergioserra.2003@gmail.com>
2022-12-01 21:29:18 +0000

    Create Data File

---
+++ src/main/java/pt/ipvc/ittalents/Data.java
@@ -0,0 +1,13 @@
+package pt.ipvc.ittalents;
+
+import pt.ipvc.ittalents.Backend.Person;
+import pt.ipvc.ittalents.Backend.Skill;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Data {
+    public static int logedUserId;
+    public static List<Person> persons = new ArrayList<>();
+    public static List<Skill> skills = new ArrayList<>();
+}
