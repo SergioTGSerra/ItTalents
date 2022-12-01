cf0b5dc8d325e574244aa451067040bac6bf6b39
tiago <sergioserra.2003@gmail.com>
2022-12-01 21:30:44 +0000

    Move ViewFactory

--- src/main/java/pt/ipvc/ittalents/ViewFactory.java
+++ src/main/java/pt/ipvc/ittalents/ViewFactory.java
@@ -1,23 +1,19 @@
-package pt.ipvc.ittalents.Views;
+package pt.ipvc.ittalents;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ViewFactory {
-
     public static void showLogin() {
         createStage("Login");
     }
-
     public static void showRegister() {
         createStage("Register");
     }
-
     public static void closeStage(Stage stage){
         stage.close();
     }
-
     private static void createStage(String file){
         FXMLLoader fxmlLoader = new FXMLLoader(ViewFactory.class.getResource("/pt/ipvc/ittalents/Fxml/" + file + ".fxml"));
         Scene scene = null;
@@ -31,5 +27,4 @@ public class ViewFactory {
         stage.setScene(scene);
         stage.show();
     }
-
 }
