2d6da4e53172e29dbf263d9b9efd0be7cb43caaa
tiago <sergioserra.2003@gmail.com>
2022-12-03 01:12:30 +0000

    Update Data.java

--- src/main/java/pt/ipvc/ittalents/Data.java
+++ src/main/java/pt/ipvc/ittalents/Data.java
@@ -4,10 +4,13 @@ import pt.ipvc.ittalents.Backend.Person;
 import pt.ipvc.ittalents.Backend.Skill;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Data {
     public static int logedUserId;
     public static List<Person> persons = new ArrayList<>();
-    public static List<Skill> skills = new ArrayList<>();
+
+    public static Map<Skill, List<Person>> mapSkillsPersons = new HashMap<Skill, List<Person>>();
 }
