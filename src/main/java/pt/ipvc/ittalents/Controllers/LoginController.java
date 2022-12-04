cb8634389edd23990118c7af35bb5172ce679029
tiago <sergioserra.2003@gmail.com>
2022-12-04 00:25:20 +0000

    Update LoginController.java

--- src/main/java/pt/ipvc/ittalents/Controllers/LoginController.java
+++ src/main/java/pt/ipvc/ittalents/Controllers/LoginController.java
@@ -7,9 +7,10 @@ import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import pt.ipvc.ittalents.Backend.Person;
-import pt.ipvc.ittalents.Data;
+import pt.ipvc.ittalents.Models.Persons;
 import pt.ipvc.ittalents.Exceptions.LoginException;
 import pt.ipvc.ittalents.ViewFactory;
+
 import java.util.Objects;
 
 public class LoginController {
@@ -18,31 +19,23 @@ public class LoginController {
     public Button goToRegisterBtn;
     public Button doLoginBtn;
     public Label infoLabel;
-
     public void goToRegister() {
         ViewFactory.closeStage((Stage)goToRegisterBtn.getScene().getWindow());
         ViewFactory.showRegister();
     }
-
-    private void doLogin() throws LoginException {
+    private Person validator() throws LoginException {
         if(username.getText().isEmpty()) throw new LoginException("The username is empty");
         if(password.getText().isEmpty()) throw new LoginException("The password is empty!");
-
-        boolean exit = false;
-        for (Person p : Data.persons) {
-            if(Objects.equals(username.getText(), p.getUsername()) && Objects.equals(password.getText(), p.getPassword())) {
-                exit = true;
-                Data.logedUserId = p.getId();
-            }
-        }
-
-        if(exit){
-            ViewFactory.closeStage((Stage)doLoginBtn.getScene().getWindow());
-            ViewFactory.showDashboard();
-        }else
-            throw new LoginException("Utilizador não encontrado!");
+        for (Person p : Persons.data)
+            if(Objects.equals(username.getText(), p.getUsername()) && Objects.equals(password.getText(), p.getPassword()))
+                return p;
+        throw new LoginException("User not found!");
     }
-
+    private void doLogin() throws LoginException {
+        Persons.logedPerson = this.validator();
+        ViewFactory.closeStage((Stage)doLoginBtn.getScene().getWindow());
+        ViewFactory.showDashboard();
+    }
     public void submitLogin() {
         try{
             this.doLogin();
