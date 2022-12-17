b09e768e37e71a014e1f88d5ea54b7578f8cdf41
Sérgio Serra <sergioserra.2003@gmail.com>
2022-12-17 21:56:46 +0000

    Update Skill.java

--- src/main/java/pt/ipvc/ittalents/Backend/Skill.java
+++ src/main/java/pt/ipvc/ittalents/Backend/Skill.java
@@ -1,23 +1,33 @@
 package pt.ipvc.ittalents.Backend;
 
+import pt.ipvc.ittalents.Models.Persons;
+import pt.ipvc.ittalents.Models.Skills;
+
 import java.io.Serializable;
 
 public class Skill implements Serializable {
+    private int id = 1;
     private String name;
     private String description;
-    private AreaType category;
+    private AreaType areaType;
 
     public Skill(){}
     
     /**
      * @param name Skill name
      * @param description Description of skill
-     * @param category Skill category in ITArea
+     * @param areaType Skill category in ITArea
      */
-    public Skill(String name, String description, AreaType category) {
+    public Skill(String name, String description, AreaType areaType) {
+        if(!Skills.data.isEmpty())
+            this.id += Skills.data.get(Skills.data.size() - 1).getId();
         this.name = name;
         this.description = description;
-        this.category = category;
+        this.areaType = areaType;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -28,7 +38,7 @@ public class Skill implements Serializable {
         return description;
     }
 
-    public AreaType getCategory() {
-        return category;
+    public AreaType getAreaType() {
+        return areaType;
     }
 }
