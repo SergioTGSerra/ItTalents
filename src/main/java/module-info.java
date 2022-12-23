c93e5161c89aba582c1fd64677246a971a774181
Sérgio Serra <sergioserra.2003@gmail.com>
2022-12-23 02:45:42 +0000

    Resolve Javafx not start

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module pt.ipvc.ittalents {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens pt.ipvc.ittalents to javafx.fxml;
     exports pt.ipvc.ittalents;
     exports pt.ipvc.ittalents.Backend;
     exports pt.ipvc.ittalents.Backend.Exceptions;
