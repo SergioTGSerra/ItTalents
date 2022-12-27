0415f2c80c12c9773cd56dc39ca370815ae94fc0
Sérgio Serra <sergioserra.2003@gmail.com>
2022-12-27 23:42:32 +0000

    Update DashboardController.java

---
+++ src/main/java/pt/ipvc/ittalents/Backend/Controllers/Client/DashboardController.java
@@ -0,0 +1,38 @@
+package pt.ipvc.ittalents.Backend.Controllers.Client;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import pt.ipvc.ittalents.Models.Persons;
+import pt.ipvc.ittalents.Models.Skills;
+import pt.ipvc.ittalents.Routes.AuthRoutes;
+import pt.ipvc.ittalents.Routes.GenericRoutes;
+import pt.ipvc.ittalents.Routes.ViewFactory;
+
+import java.io.IOException;
+
+public class DashboardController {
+    public Label usernameLabel;
+
+    public void initialize(){
+        try{
+            Skills.loadData();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        usernameLabel.setText(Persons.loged.getUsername());
+    }
+
+    public void logout() {
+        ViewFactory.closeStage((Stage)usernameLabel.getScene().getWindow());
+        Persons.loged = null;
+        AuthRoutes.showLogin();
+    }
+
+    public void goToFindTalents() {
+        GenericRoutes.showSearchTalents();
+    }
+
+    public void goToRegistProject() {
+    }
+}
