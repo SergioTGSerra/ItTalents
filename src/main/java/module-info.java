78c946b74a57a8bc5fc2340357315bf44e1d776c
Sérgio Serra <sergioserra.2003@gmail.com>
2022-12-27 23:41:41 +0000

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,9 @@ module pt.ipvc.ittalents {
     exports pt.ipvc.ittalents.Backend;
     exports pt.ipvc.ittalents.Backend.Exceptions;
     exports pt.ipvc.ittalents.Backend.Controllers.Professional;
+    exports pt.ipvc.ittalents.Backend.Controllers.Admin;
+    exports pt.ipvc.ittalents.Backend.Controllers.Client;
+    exports pt.ipvc.ittalents.Backend.Controllers;
     exports pt.ipvc.ittalents.Models;
     exports pt.ipvc.ittalents.Routes;
     exports pt.ipvc.ittalents.Backend.Controllers.Auth;
