a2d09d02797420d3e6cd9af58eaa5fb24a1f4fdf
Sérgio Serra <sergioserra.2003@gmail.com>
2022-12-27 23:42:05 +0000

    Update GenericRoutes.java

---
+++ src/main/java/pt/ipvc/ittalents/Routes/GenericRoutes.java
@@ -0,0 +1,7 @@
+package pt.ipvc.ittalents.Routes;
+
+public abstract class GenericRoutes extends ViewFactory {
+    public static void showSearchTalents() {
+        createStage("Generic/SearchTalents");
+    }
+}
