2c995cc82a8ffc47a576622903e877a18c7f65d0
Sérgio Serra <sergioserra.2003@gmail.com>
2022-12-28 18:24:13 +0000

    Update Persons.java

--- src/main/java/pt/ipvc/ittalents/Models/Persons.java
+++ src/main/java/pt/ipvc/ittalents/Models/Persons.java
@@ -3,9 +3,12 @@ package pt.ipvc.ittalents.Models;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 import pt.ipvc.ittalents.Backend.Person;
 
 public abstract class Persons {
+    public static int index;
     public static Person loged;
     public static List<Person> data = new ArrayList<>();
     public static void saveData() throws IOException {
@@ -13,6 +16,10 @@ public abstract class Persons {
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(data);
         oos.close();
+
+        PrintWriter writer = new PrintWriter(System.getProperty("user.dir") + "\\src\\main\\java\\pt\\ipvc\\ittalents\\SavedData\\PersonsI.data");
+        writer.print(index);
+        writer.close();
     }
 
     public static void loadData() throws IOException, ClassNotFoundException {
@@ -20,6 +27,9 @@ public abstract class Persons {
         ObjectInputStream ois = new ObjectInputStream(fis);
         data = (List<Person>) ois.readObject();
         ois.close();
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\src\\main\\java\\pt\\ipvc\\ittalents\\SavedData\\PersonsI.data"));
+        index = Integer.parseInt(bufferedReader.readLine());
     }
 
     public static void updatePersons(){
