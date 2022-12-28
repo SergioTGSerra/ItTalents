818e63971b785c93728c74daa44c5ef018c29714
Sérgio Serra <sergioserra.2003@gmail.com>
2022-12-28 16:39:33 +0000

    Update DashboardController.java

--- src/main/java/pt/ipvc/ittalents/Backend/Controllers/Admin/DashboardController.java
+++ src/main/java/pt/ipvc/ittalents/Backend/Controllers/Admin/DashboardController.java
@@ -1,10 +1,12 @@
 package pt.ipvc.ittalents.Backend.Controllers.Admin;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import pt.ipvc.ittalents.Backend.Skill;
 import pt.ipvc.ittalents.Models.Persons;
 import pt.ipvc.ittalents.Models.Skills;
+import pt.ipvc.ittalents.Routes.AdminRoutes;
 import pt.ipvc.ittalents.Routes.AuthRoutes;
 import pt.ipvc.ittalents.Routes.ViewFactory;
 
@@ -26,15 +28,19 @@ public class DashboardController {
         nSkills.setText("Number of Skills: " + Skills.data.size());
     }
 
-    public void manageUsers() {
-    }
-
     public void manageSkills() {
+        ViewFactory.closeStage((Stage) usernameLabel.getScene().getWindow());
+        AdminRoutes.showManageSkills();
     }
 
     public void logout() {
         ViewFactory.closeStage((Stage)usernameLabel.getScene().getWindow());
         Persons.loged = null;
         AuthRoutes.showLogin();
+    }
+
+    public void goToManageUsers() {
+        ViewFactory.closeStage((Stage) usernameLabel.getScene().getWindow());
+        AdminRoutes.showManageUsers();
     }
 }
