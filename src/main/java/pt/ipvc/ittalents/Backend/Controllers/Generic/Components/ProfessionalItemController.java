53fdcb08d5ad1602e4c4be369182ee76f881cb09
Sérgio Serra <sergioserra.2003@gmail.com>
2022-12-29 03:45:51 +0000

    Update ProfessionalItemController.java

---
+++ src/main/java/pt/ipvc/ittalents/Backend/Controllers/Generic/Components/ProfessionalItemController.java
@@ -0,0 +1,37 @@
+package pt.ipvc.ittalents.Backend.Controllers.Generic.Components;
+
+import javafx.scene.control.Label;
+import pt.ipvc.ittalents.Backend.Professional;
+import pt.ipvc.ittalents.Backend.Skill;
+import pt.ipvc.ittalents.Models.Skills;
+
+public class ProfessionalItemController {
+    public Label name;
+    public Label skills;
+    public Label itArea;
+    public Label country;
+    public Label price;
+    Professional p;
+
+    public void setData(Professional p){
+        this.p = p;
+        name.setText(p.getName());
+        itArea.setText("IT Area: " + p.getiTArea().toString());
+        country.setText("Country: " + p.getCountry());
+        price.setText("Price: " + p.getPriceHour() + "€");
+        StringBuilder sb = new StringBuilder();
+        for(Integer i : p.getSkills().keySet())
+            sb.append(getSkillName(i)).append(", ");
+        skills.setText("Skills: " + sb);
+    }
+
+    private String getSkillName(int skillId){
+        for(Skill s : Skills.data)
+            if(s.getId() == skillId)
+                return s.getName();
+        return null;
+    }
+
+    public void makeOffer() {
+    }
+}
