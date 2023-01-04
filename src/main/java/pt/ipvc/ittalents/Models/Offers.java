07683088b3faabe3ba968d06b0d7cf46e25db9cb
Sérgio Serra <sergioserra.2003@gmail.com>
2023-01-04 22:59:50 +0000

    Update Models

---
+++ src/main/java/pt/ipvc/ittalents/Models/Offers.java
@@ -0,0 +1,32 @@
+package pt.ipvc.ittalents.Models;
+
+import pt.ipvc.ittalents.Backend.Offer;
+import pt.ipvc.ittalents.Backend.Person;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Offers {
+    public static int index;
+    public static Map<Offer, Person> data = new HashMap<>();
+
+    public static void saveData() throws IOException {
+        FileOutputStream fos = new FileOutputStream(System.getProperty("user.dir") + "\\src\\main\\java\\pt\\ipvc\\ittalents\\SavedData\\Offers.data");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(data);
+        oos.close();
+
+        PrintWriter writer = new PrintWriter(System.getProperty("user.dir") + "\\src\\main\\java\\pt\\ipvc\\ittalents\\SavedData\\OffersI.data");
+        writer.print(index);
+        writer.close();
+    }
+    public static void loadData() throws IOException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\main\\java\\pt\\ipvc\\ittalents\\SavedData\\Offers.data");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        data = (Map<Offer, Person>) ois.readObject();
+        ois.close();
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\src\\main\\java\\pt\\ipvc\\ittalents\\SavedData\\OffersI.data"));
+        index = Integer.parseInt(bufferedReader.readLine());
+    }
+}
--- src/main/java/pt/ipvc/ittalents/Models/Persons.java
+++ src/main/java/pt/ipvc/ittalents/Models/Persons.java
@@ -3,7 +3,6 @@ package pt.ipvc.ittalents.Models;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import pt.ipvc.ittalents.Backend.Person;
 
